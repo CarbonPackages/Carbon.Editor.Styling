cb6874b14be1315447895407b147b1d321823836
Jon Uhlmann <account@uhlmann.pro>
2024-12-10 00:29:29 +0100

    Update: Export also some components

--- Resources/Private/Editor/index.js
+++ Resources/Private/Editor/index.js
@@ -2,5 +2,8 @@ import Border from "./Border";
 import BorderRadius from "./BorderRadius";
 import Spacing from "./Spacing";
 import TextAreaWithCounter from "./TextAreaWithCounter";
+import Dialog from "./Components/Dialog";
+import TextInput from "./Components/TextInput";
+import Dropdown from "./Components/Dropdown";
 
-export { Border, BorderRadius, Spacing, TextAreaWithCounter };
+export { Border, BorderRadius, Spacing, TextAreaWithCounter, Dialog, TextInput, Dropdown };
